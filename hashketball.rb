62b7a0c614f9afca343eff155a63acf7db57d7ec
Craig Iturbe <craigiturbe@Craigs-Air.home>
2016-09-27 20:30:57 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -160,12 +160,11 @@ def team_names
   end
 end
 
-def player_numbers (team_name)
-  output = []
-  game_hash.each_with_object([] )do |(status, team), numbers_array|
+def player_numbers(team_name)
+  game_hash.each_with_object([])do |(status, team), output|
     if team[:team_name] == team_name
       team[:players].each do |name, stats|
-        numbers_array << stats[:number]
+        output << stats[:number]
       end
     end
   end
